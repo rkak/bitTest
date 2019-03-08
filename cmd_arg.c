815ebe510839c0f650c7188eefc7e4cd5bea52c9
rkak <micawal@hanmail.net>
2019-03-08 14:28:52 +0900

    create cmd_arg.c

---
+++ cmd_arg.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+extern char ** environ;
+
+int main(int argc, char *argv[]){
+	setenv("mydir", "/home/hw", 0);
+	setenv("kdrenel_src = %s\n", getenv("KERNEL_SRC"));
+
+	/*
+	while(*environ){
+		printf("%s\n", *environ++);
+	}
+	*/
+	sleep(10);
+	return 0;
+}
